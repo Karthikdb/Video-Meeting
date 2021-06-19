ff17877ceeac3a36cb99047bf465082d51fedab9
karthikdb <karthikbaskaran555@gmail.com>
2021-06-19 19:31:36 +0530

    build

---
+++ build/precache-manifest.a51aa9674aa1189131011763a704fdf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ba803aa551e71621161d5f46b022a69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f530f6e46a66cdbdb166",
+    "url": "/static/css/2.a3c8e502.chunk.css"
+  },
+  {
+    "revision": "17ad9a54d54ac1b363e1",
+    "url": "/static/css/main.2a3e2305.chunk.css"
+  },
+  {
+    "revision": "f530f6e46a66cdbdb166",
+    "url": "/static/js/2.c3bd581d.chunk.js"
+  },
+  {
+    "revision": "ed344764e96e72a32d0bd51aea29b8b2",
+    "url": "/static/js/2.c3bd581d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17ad9a54d54ac1b363e1",
+    "url": "/static/js/main.74eee024.chunk.js"
+  },
+  {
+    "revision": "5ff6df50c1a535018958",
+    "url": "/static/js/runtime-main.df338948.js"
+  }
+]);
